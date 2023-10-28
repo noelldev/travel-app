7a3ea43c61356bd26bbef2aaf603e5e7a0c1de35
kodrr <ntlauigan@gmail.com>
2023-10-28 11:37:29 +0800

    add assets

---
+++ components/Navbar.tsx
@@ -0,0 +1,12 @@
+import Image from 'next/image';
+import Link from 'next/link';
+
+const Navbar = () => {
+  return (
+    <nav className="border-2 border-red-500 flexBetween max-container padding-container relative z-30 py-5">
+      <Link href="/">Home</Link>
+      <Image src="hilink-logo.svg" alt="logo" width={74} height={29} />
+    </nav>
+  );
+};
+export default Navbar;
